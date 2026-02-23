4ead5b5073cdb5926d1fb48f5cc16bcadd3b54f3
Shane Loretz <shane.loretz@gmail.com>
2026-02-23 11:24:08 -0800

    Better printing
    Signed-off-by: Shane Loretz <shane.loretz@gmail.com>

--- src/chainglogs/ai.py
+++ src/chainglogs/ai.py
@@ -7,6 +7,39 @@ from google import genai
 from google.genai import errors
 
 
+_PROMPT = """\
+You are an experienced tech writer.
+Summarize one commit message as a single line of reStructuredText suitable for a CHANGELOG.rst entry.
+
+* Use active voice
+* Be concise
+* Preserve existing links
+* Output a single line
+
+Correct examples:
+
+```
+Add bool return to ``_base_logger`` function (`#24 <https://github.com/hazel/watership/issues/24>`_)
+```
+
+```
+Workaround race condition when closing the socket (`#1337 <https://github.com/ros2/rclpy/issues/1337>`_)
+```
+
+```
+Fix "TypeError: not enough arguments for format string" (`#632 <https://github.com/sloretz/lidar_viewer/issues/632>`_)
+```
+
+Summarize this commit message:
+"""
+
+
+def indent(text: str) -> str:
+    """Return text with an indent on all lines."""
+    four_spaces = "    "
+    return four_spaces + text.replace("\n", f"\n{four_spaces}").strip()
+
+
 class AI:
     """Handles interactions with the Gemini API."""
 
@@ -17,18 +50,14 @@ class AI:
 
     @staticmethod
     def _get_gemini_api_key() -> Optional[str]:
-        if 'GEMINI_API_KEY' in os.environ:
-            return os.environ['GEMINI_API_KEY']
+        if "GEMINI_API_KEY" in os.environ:
+            return os.environ["GEMINI_API_KEY"]
         return keyring.get_password("chainglogs", "gemini-api-key")
 
     def summarize_commit(self, message: str) -> str:
-        prompt = (
-            "Summarize the following commit message into a single, concise line "
-            "of reStructuredText suitable for a changelog entry. Preserve any "
-            "existing reStructuredText links. Do not include bullet points.\n\n"
-            f"Commit message:\n{message}"
-        )
-        print(f"Summarizing commit\n{message}")
+        prompt = _PROMPT + message
+        print(f"Summarizing commit")
+        print(indent(message))
 
         while True:
             try:
@@ -36,7 +65,8 @@ class AI:
                     model="gemini-2.5-flash-lite", contents=prompt
                 )
                 summary = response.text.strip()
-                print("Summary:\n{summary}")
+                print(f"Summary")
+                print(indent(summary))
                 return summary
 
             except errors.ClientError as e:
@@ -56,7 +86,7 @@ class AI:
         """Parses the retryDelay from the ClientError response details."""
         default_delay = 30.0
 
-        for detail in exc.details['error']['details']:
+        for detail in exc.details["error"]["details"]:
             if detail.get("@type") == "type.googleapis.com/google.rpc.RetryInfo":
                 delay_str = detail.get("retryDelay", "30s")
                 # Remove the 's' suffix and convert to float
